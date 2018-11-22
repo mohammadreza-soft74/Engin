5dd416d4f05071d437a279468d09bb3c89919854
Mohammadreza Tofigh <mohammadreza.soft74@gmail.com>
2018-11-22 13:01:21 +0330

    add BasePageLoad class add PageLoad function

---
+++ app/Clasess/Base/MainActions/BasePageLoad/BasePageLoad.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mohammadreza
+ * Date: 11/22/18
+ * Time: 12:35 PM
+ */
+
+namespace App\Clasess\Base\MainActions\BasePageLoad;
+
+use App\Clasess\Base\Managers\KeyManager\KeyManager;
+use App\Clasess\Base\Managers\ContainerManager\ContainerManager;
+
+
+class BasePageLoad
+{
+    /**
+     * @brief on moodle page load this function just check container availability and its state
+     * @param $data
+     * @return bool
+     * @throws \Exception
+     */
+    protected function PageLoad($data)
+    {
+
+        $key = $data->key;
+
+        if (!$containerId = KeyManager::checkContainerIdWithKey($key))
+            throw new \Exception("Error: Container is not available \n.check Key " );
+
+        if (!ContainerManager::getContainerState($containerId))
+            ContainerManager::startContainer($containerId);
+
+        KeyManager::updateTimeStamp($key);
+
+        return $containerId;
+    }
+}
