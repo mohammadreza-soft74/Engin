7c8140cb800e93d4d7317ce1dda8cafe5d37a4b7
Mohammadreza Tofigh <mohammadreza.soft74@gmail.com>
2018-11-22 11:02:41 +0330

    add redis static function to creat an object of redis client to connect to redis database

--- app/Clasess/Base/Memory/RedisClientFactory.php
+++ app/Clasess/Base/Memory/RedisClientFactory.php
@@ -11,5 +11,42 @@ namespace App\Clasess\Base\Memory;
 
 class RedisClientFactory
 {
+    /**
+     * @brief make an instance of Redis client
+     * @param $type
+     * @return Client
+     */
+    public static function redis($type)
+    {
+
+
+        switch ($type)
+        {
+
+            case ("key"): return new Client(array(
+                'scheme'   => 'tcp',
+                'host'     => '127.0.0.1',
+                'port'     => 6379,
+                'database' => 1
+            ));
+                break;
+
+            case ("path"): return new Client(array(
+                'scheme'   => 'tcp',
+                'host'     => '127.0.0.1',
+                'port'     => 6379,
+                'database' => 2
+            ));
+
+            case ("config"): return new Client(array(
+                'scheme'   => 'tcp',
+                'host'     => '127.0.0.1',
+                'port'     => 6379,
+                'database' => 3
+            ));
+        }
+
+
+    }
 
 }
