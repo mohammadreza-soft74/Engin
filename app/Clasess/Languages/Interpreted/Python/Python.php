b50fa195ccabb67a3fab3712f0f1176274fb9e6c
Mohammadreza Tofigh <mohammadreza.soft74@gmail.com>
2018-11-24 11:45:13 +0330

    add python class to do main actions add create function to create python container

---
+++ app/Clasess/Languages/Interpreted/Python/Python.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mohammadreza
+ * Date: 11/24/18
+ * Time: 11:08 AM
+ */
+
+namespace App\Clasess\Languages\Interpreted\Python;
+
+
+use App\Clasess\Languages\Interpreted\Python\MainActions\Create\Create;
+use App\Clasess\Languages\Interpreted\Python\MainActions\PageLoad\PageLoad;
+use App\Clasess\Languages\Interpreted\Python\MainActions\Run\Run;
+
+class Python
+{
+
+    /**
+     * @param $request
+     * @return array
+     * @throws \Exception
+     */
+    public function create($request)
+    {
+        $create = new Create();
+        $result = $create->createContainer($request);
+
+        return $result;
+    }
+
+
+}
