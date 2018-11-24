40d7aac9a5460ebf59ccc5af1e808711b0da5495
Mohammadreza Tofigh <mohammadreza.soft74@gmail.com>
2018-11-24 10:41:56 +0330

    add generateRunError function to capture all error on app and write errors in log file

--- app/Http/Controllers/Engine.php
+++ app/Http/Controllers/Engine.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Clasess\Base\Managers\KeyManager\KeyManager;
 use App\Clasess\Base\RequestValidate\RequestValidate;
+use App\Clasess\Calender\Calender;
+use DateTime;
+use DateTimeZone;
 
 class Engine extends Controller
 {
@@ -18,14 +21,16 @@ class Engine extends Controller
 
         try{
 
-            $key = $request->key;
-            $request = RequestValidate::createValidator($request);
 
-            $courseConfig = KeyManager::getCourseConfig($request->key);
+
+            $req = RequestValidate::createValidator($request);
+            $key = $req["key"];
+
+            $courseConfig = KeyManager::getCourseConfig($key);
 
 
             $languageActions = new $courseConfig["LanguageActions"];
-            $result = $languageActions->create($request);
+            $result = $languageActions->create($req);
 
             $resultHandler = new $courseConfig["ResultHandler"];
             $result = $resultHandler->create($result);
@@ -37,6 +42,29 @@ class Engine extends Controller
 
             return $this->generateRunError($e->getMessage()." | ". $e->getFile() . " | ". $e->getLine(),$key);
         }
+    }
 
+
+    function generateRunError($message,$key)
+    {
+
+        $persianDate = new Calender();
+        $now = now();
+        $year= $now->year;
+        $month = $now->month;
+        $day = $now->day;
+        $time = new DateTime(null, new DateTimeZone('Asia/tehran'));
+
+
+        $time = $time->format("H:i:s");
+        $date = $persianDate->gregorian_to_jalali($year, $month, $day ,"/");
+
+
+        $error = "$message\n\n|time>> $time <<time| ----- |Date>> $date <<Date| \n******************************************************************\n";
+        error_log($error,3,"/home/mohammadreza/violin/log/$key");
+        return [
+            "error" => true,
+            "message" => $message
+        ];
     }
 }
