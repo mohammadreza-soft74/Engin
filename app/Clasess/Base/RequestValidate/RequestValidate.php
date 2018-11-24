8bbbeeb0ce11f8eb6d61f8aa7c8ea335cd651c12
Mohammadreza Tofigh <mohammadreza.soft74@gmail.com>
2018-11-24 13:25:39 +0330

    add runValidator function to validate run requests

--- app/Clasess/Base/RequestValidate/RequestValidate.php
+++ app/Clasess/Base/RequestValidate/RequestValidate.php
@@ -10,6 +10,7 @@ namespace App\Clasess\Base\RequestValidate;
 
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use Config;
 
 
 class RequestValidate
@@ -58,10 +59,59 @@ class RequestValidate
             'key'=>$request->key,
             'path'=>$request->path
         ];
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     * @throws \Exception
+     */
+    public static function runValidator(Request $request)
+    {
+        // Empty response object
+        $ret = [];
+
+        // Form Validations
+        $validator = Validator::make($request->all(), [
+            'path' => 'required|string',
+            'files' => 'required|json',
+            'key' => 'required|string'
+        ]);
+
+        if ($validator->fails())
+            throw  new \Exception($validator->messages());
+
+        // Extra Validations
+        $files_decoded = json_decode($request->get("files"));
+        $maxFilesCount = Config::get('api.max_files_count', 10);
+        if (count($files_decoded) > $maxFilesCount)
+            throw  new \Exception("Number of files is more than " . $maxFilesCount . ".");
+
+        $maxFileSize = Config::get('api.max_file_size', 1000);
+        $files = [];
+        foreach ($files_decoded as $value) {
+            // TODO: regex should be more subtle! Also it should support '/' for creating files inside folders
+            $validator = Validator::make((array)$value, [
+                'name' => 'required|string|regex:/[a-zA-Z0-9_.\-]+/u|max:50',
+                'content' => 'required|string|max:' . $maxFileSize
+            ]);
+
+            if ($validator->fails())
+                throw  new \Exception($validator->messages());
 
 
+            $files[] = [
+                "name" => $value->name,
+                "content" => $value->content
+            ];
+        }
 
+        // {// Prepare object to be returned
+        $ret["path"] = $request->path;
+        $ret["key"] = $request->key;
+        $ret["files"] = $files;
+        $ret['requestType'] = 'run';
 
-
+        return $ret;
     }
 }
