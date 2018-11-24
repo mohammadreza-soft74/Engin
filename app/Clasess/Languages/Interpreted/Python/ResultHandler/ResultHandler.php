402a405a5817d4b6918cedf130390f7a2062ce28
Mohammadreza Tofigh <mohammadreza.soft74@gmail.com>
2018-11-24 11:38:42 +0330

    add ResultHandler derived BaseResultHandler

---
+++ app/Clasess/Languages/Interpreted/Python/ResultHandler/ResultHandler.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mohammadreza
+ * Date: 11/24/18
+ * Time: 10:49 AM
+ */
+
+namespace App\Clasess\Languages\Interpreted\Python\ResultHandler;
+
+
+use App\Clasess\Base\BaseResultHandler\BaseResultHandler;
+
+class ResultHandler extends BaseResultHandler
+{
+
+}
