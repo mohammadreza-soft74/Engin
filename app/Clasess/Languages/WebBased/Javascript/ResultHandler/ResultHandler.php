3ab7adfded56f65496135295d761d60e4975e4bf
Mohammadreza Tofigh <mohammadreza.soft74@gmail.com>
2018-11-26 13:00:05 +0330

    add ResultHandler class to handle eah action result derived BaseResultHandler

---
+++ app/Clasess/Languages/WebBased/Javascript/ResultHandler/ResultHandler.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mohammadreza
+ * Date: 11/26/18
+ * Time: 11:29 AM
+ */
+
+namespace App\Clasess\Languages\WebBased\Javascript\ResultHandler;
+
+use App\Clasess\Base\BaseResultHandler\BaseResultHandler;
+
+class ResultHandler extends BaseResultHandler
+{
+
+}
