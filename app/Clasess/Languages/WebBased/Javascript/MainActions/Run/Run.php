c3ebd342d0151c99ea04085984f1771f78dd3522
Mohammadreza Tofigh <mohammadreza.soft74@gmail.com>
2018-11-26 12:58:58 +0330

    add Run class to run javascript code derived BaseRun class

---
+++ app/Clasess/Languages/WebBased/Javascript/MainActions/Run/Run.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mohammadreza
+ * Date: 11/26/18
+ * Time: 11:29 AM
+ */
+
+namespace App\Clasess\Languages\WebBased\Javascript\MainActions\Run;
+
+
+use App\Clasess\Base\MainActions\BaseRun\BaseRun;
+
+class Run extends BaseRun
+{
+    /**
+     * @param $data
+     * @return mixed
+     * @throws \Exception
+     */
+    public function run($data)
+    {
+        return parent::run($data);
+    }
+}
