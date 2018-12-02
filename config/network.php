d481ae82a8fa8a5a9b89f5d6ddd731619d789c3e
Mohammadreza Tofigh <mohammadreza.soft74@gmail.com>
2018-12-02 14:57:29 +0330

    add network config file that contain network configs such as allowed ip addresses to send request to Engine

---
+++ config/network.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mohammadreza
+ * Date: 12/2/18
+ * Time: 2:45 PM
+ */
+
+return[
+
+    'request_allowed_ips'=>[
+        '192.168.50.55',
+        '91.98.50.86',
+
+    ],
+];
