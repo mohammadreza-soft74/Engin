0a857172e1b543c5c2f5797e851fed23f6f08f84
Mohammadreza Tofigh <mohammadreza.soft74@gmail.com>
2018-12-25 12:10:57 +0330

    add redid config file

---
+++ config/redis.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mohammadreza
+ * Date: 12/25/18
+ * Time: 12:00 PM
+ */
+
+return[
+  "redis_conf"=>[
+      "ip"=>"127.0.0.1",
+      "port"=>6379,
+      "password"=>"m0t0r123"
+  ]
+];
