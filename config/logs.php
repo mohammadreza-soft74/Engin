310df37ee9442440ecbef225d58d49490ee42304
Mohammadreza Tofigh <mohammadreza.soft74@gmail.com>
2018-12-25 12:18:12 +0330

    add logs config file

---
+++ config/logs.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mohammadreza
+ * Date: 12/25/18
+ * Time: 12:11 PM
+ */
+
+return[
+
+    "error"=>[
+        "path"=> "/home/mohammadreza/violin/log/"
+    ],
+
+    "stop"=>[
+        "path"=> "/home/mohammadreza/violin/stopLog"
+    ]
+];
