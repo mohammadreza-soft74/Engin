29fae6e7efe0d24feca3348b05d3362244901157
Mohammadreza Tofigh <mohammadreza.soft74@gmail.com>
2018-12-31 11:11:16 +0330

    make validation stronger

--- app/Clasess/Base/RequestValidate/RequestValidate.php
+++ app/Clasess/Base/RequestValidate/RequestValidate.php
@@ -16,28 +16,32 @@ use Config;
 class RequestValidate
 {
     /**
-     * @brief validate create Request
+     * @brief validate create Request.
+     *
      * @param Request $request
-     * @return Request
+     * @return array
      * @throws \Exception
      */
     public static function createValidator(Request $request)
     {
-
         $validator = Validator::make($request->all(), [
-            'key' => 'required|string'
-
+            'key' => 'required|string|regex:/^[1-2]+[-][0-9]+/'
         ]);
+
         if ($validator->fails())
             throw  new \Exception($validator->messages());
 
-        return [
-            'key' => $request->key,
+
+        return[
+            'key'=>$request->key,
+            'path'=>$request->path
         ];
 
     }
 
     /**
+     * @brief validate pageLoad Request.
+     *
      * @param Request $request
      * @return array
      * @throws \Exception
@@ -46,8 +50,8 @@ class RequestValidate
     {
 
         $validator = Validator::make($request->all(), [
-            'path' => 'required|string',
-            'key' => 'required|string'
+            'path' => 'required|string|regex: /^\/[a-zA-Z0-9\-]+\/[a-zA-Z0-9\-]+$/',
+            'key' => 'required|string|regex:/^[1-2]+[-][0-9]+/'
         ]);
 
         if ($validator->fails())
@@ -62,6 +66,8 @@ class RequestValidate
     }
 
     /**
+     * @brief validate run Request.
+     *
      * @param Request $request
      * @return array
      * @throws \Exception
@@ -73,9 +79,9 @@ class RequestValidate
 
         // Form Validations
         $validator = Validator::make($request->all(), [
-            'path' => 'required|string',
+            'path' => 'required|string|regex: /^\/[a-zA-Z0-9\-]+\/[a-zA-Z0-9\-]+$/',
             'files' => 'required|json',
-            'key' => 'required|string'
+            'key' => 'required|string|regex:/^[1-2]+[-][0-9]+/'
         ]);
 
         if ($validator->fails())
@@ -83,17 +89,17 @@ class RequestValidate
 
         // Extra Validations
         $files_decoded = json_decode($request->get("files"));
-        $maxFilesCount = Config::get('api.max_files_count', 10);
+        $maxFilesCount = Config::get('languages_config.max_files_count', 10);
         if (count($files_decoded) > $maxFilesCount)
             throw  new \Exception("Number of files is more than " . $maxFilesCount . ".");
 
-        $maxFileSize = Config::get('api.max_file_size', 1000);
+        $maxFileSize = Config::get('languages_config.max_file_size', 1000);
         $files = [];
         foreach ($files_decoded as $value) {
             // TODO: regex should be more subtle! Also it should support '/' for creating files inside folders
             $validator = Validator::make((array)$value, [
                 'name' => 'required|string|regex:/[a-zA-Z0-9_.\-]+/u|max:50',
-                'content' => 'required|string|max:' . $maxFileSize
+                'content' => 'string|max:' . $maxFileSize
             ]);
 
             if ($validator->fails())
@@ -116,6 +122,8 @@ class RequestValidate
     }
 
     /**
+     * @brief validate reset and final Request.
+     *
      * @param Request $request
      * @return array
      * @throws \Exception
@@ -124,8 +132,8 @@ class RequestValidate
     {
 
         $validator = Validator::make($request->all(), [
-            'path' => 'required|string',
-            'key' => 'required|string'
+            'path' => 'required|string|regex: /^\/[a-zA-Z0-9\-]+\/[a-zA-Z0-9\-]+$/',
+            'key' => 'required|string|regex:/^[1-2]+[-][0-9]+/'
         ]);
         if ($validator->fails())
             throw  new \Exception($validator->messages());
@@ -136,6 +144,13 @@ class RequestValidate
         ];
     }
 
+    /**
+     * @brief validate update Request.
+     *
+     * @param Request $request
+     * @return array
+     * @throws \Exception
+     */
     public static function updateRequestValidator(Request $request)
     {
 
