949e5f5c95873d14e88bd80faf629f1e5e1c5255
Mohammadreza Tofigh <mohammadreza.soft74@gmail.com>
2019-01-29 15:46:11 +0330

    some additional codes  removed

--- app/Clasess/Base/Memory/RedisClientFactory.php
+++ app/Clasess/Base/Memory/RedisClientFactory.php
@@ -16,46 +16,25 @@ class RedisClientFactory
     /**
      * @brief make an instance of Redis client.
      *
-     * @param $type
      * @return Client
      */
-    public static function redis($type)
+    public static function redis()
     {
 
         $ip = Config::get('redis.redis_conf.ip');
         $port = Config::get('redis.redis_conf.port');
         $password = Config::get('redis.redis_conf.password');
 
-        switch ($type)
-        {
 
-            case ("key"): return new Client(array(
+
+            return new Client(array(
                 'scheme'   => 'tcp',
                 'host'     => $ip,
                 'port'     => $port,
                 'database' => 1,
                 'password' => $password,
             ));
-                break;
-
-            case ("path"): return new Client(array(
-                'scheme'   => 'tcp',
-                'host'     => $ip,
-                'port'     => $port,
-                'database' => 2,
-                'password' => $password,
-            ));
-
-            case ("config"): return new Client(array(
-                'scheme'   => 'tcp',
-                'host'     => $ip,
-                'port'     => $port,
-                'database' => 3,
-                'password' => $password,
-            ));
-        }
 
 
     }
-
 }
