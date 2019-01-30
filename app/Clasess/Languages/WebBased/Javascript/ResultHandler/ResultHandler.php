e45a2949228fda08929be4da068ba75efef65d18
Mohammadreza Tofigh <mohammadreza.soft74@gmail.com>
2019-01-30 10:19:54 +0330

    return result like other languges. In the future will change

--- app/Clasess/Languages/WebBased/Javascript/ResultHandler/ResultHandler.php
+++ app/Clasess/Languages/WebBased/Javascript/ResultHandler/ResultHandler.php
@@ -12,5 +12,40 @@ use App\Clasess\Base\BaseResultHandler\BaseResultHandler;
 
 class ResultHandler extends BaseResultHandler
 {
+	public function create($result)
+	{
 
+		if ($result['result'])
+		{
+			$res=[
+				'error' => false,
+				'result' => ['running'=>true]
+			];
+		}
+		return $res;
+	}
+
+	public function pageLoad($result)
+	{
+		return[
+			'error' => false,
+			'result' => $result['files']
+		];
+	}
+
+	public function run($result)
+	{
+		return[
+			'error' => false,
+			'result' => $result['path']
+		];
+	}
+
+	public function codes($result)
+	{
+		return[
+			'error' => false,
+			'result'=> $result['result']
+		];
+	}
 }
