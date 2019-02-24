b3d4a8cee7b23e5003d4a3f697b06b2f4ed71e35
Mohammadreza Tofigh <mohammadreza.soft74@gmail.com>
2019-02-24 10:17:31 +0330

    change getFiles() function to get files recursively.add comment to functions

--- app/Clasess/Base/Managers/FileManager/FileManager.php
+++ app/Clasess/Base/Managers/FileManager/FileManager.php
@@ -11,49 +11,63 @@ namespace App\Clasess\Base\Managers\FileManager;
 
 class FileManager
 {
+
 	/**
-	 * @brief get file from host
-	 *
-	 * @param $path
+	 * @brief get files from server.
+	 * 
+	 * @param $dir
+	 * @param array $results
+	 * @param array $content
 	 * @return array
 	 * @throws \Exception
 	 */
-	public static function getFiles($path)
+	public static function getFiles($dir, &$results = array(), &$content = array())
 	{
 
-		$content = [];
-
-		if (!is_dir($path))
+		if (!is_dir($dir))
 			throw  new \Exception("directory is not available !");
+		$files = scandir($dir);
 
-		if (!$dh = opendir($path))
-			throw  new \Exception("opendir result must be Resource!");
+		foreach($files as  $value){
 
-		while ($file = readdir($dh)) {
+			$path = realpath($dir.DIRECTORY_SEPARATOR.$value);
 
-			if ($file == ".." or $file == ".")
-				continue;
+			if(!is_dir($path)) {
 
-			$input = file_get_contents($path . "/" . $file);
+				$results[] = $path;
+				$ret_path =array_filter(explode('/',$path)); //remove empty elements
+				$search_result = preg_grep("/^page-[0-9]+/",$ret_path); //search for page-[0-9] with regex
 
-			if ($input == "")
-				$input = " ";
+				if ($search_result == null) //null mean page-[0-9] word not used in path
+					throw new \Exception("Error: lesson default files path syntax is not valid!\nyour syntax:$path\n(valid syntax: ../topic-1/page-2)");
+				if (count($search_result)>1)
+					throw new \Exception("Error: don't use 'page-[0-9]+' word in path More than once!");
 
-			//if ($input == null)
-			// continue;
+				$page_word_index = array_keys($search_result); //return index of page-[0-9] word in array
+				$ret_path = array_splice($ret_path,$page_word_index[0]); //return array from specified key to last
+				$file = implode('/',$ret_path);//concat array elements to make needed path
+				$value = file_get_contents($path);//get given path content
+				$content[] = ["file" => $file, "content" => $value];
 
-			$content[] = ["file" => $file, "content" => $input];
+
+
+			} else if($value != "." && $value != "..") {
+				self::getFiles($path, $results, $content);
+			}
 		}
 
 		return $content;
 	}
 
 
-
+	/**
+	 * @param $files
+	 * @param $baseDir
+	 * @return bool
+	 * @throws \Exception
+	 */
 	public static function createFiles($files, $baseDir)
 	{
-
-		// TODO: Use better validation idea
 		// TODO: Support for files in folders
 
 		// Check if it is an array
@@ -87,6 +101,11 @@ class FileManager
 	}
 
 
+	/**
+	 * recursive copy files from source to destination
+	 * @param $src
+	 * @param $dst
+	 */
 	public static function recurse_copy($src,$dst)
 	{
 		$dir = opendir($src);
@@ -106,6 +125,12 @@ class FileManager
 		closedir($dir);
 	}
 
+
+	/**
+	 * delete files in path
+	 * @param $path
+	 * @throws \Exception
+	 */
 	public  static function deleteFilesInDirectory($path)
 	{
 
